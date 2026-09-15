e8614450dcea851e9523b580cdff4a9a086090a1
Josh Kahlbaugh <joshk7683@gmail.com>
2026-09-14 19:47:21 -0700

    Add: minimum-number-of-swaps-to-make-the-string-balanced - submission-2

---
+++ Data Structures & Algorithms/minimum-number-of-swaps-to-make-the-string-balanced/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minSwaps(String s) {
+        int close = 0, maxClose = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[') close--;
+            else close++;
+            maxClose = Math.max(maxClose, close);
+        }
+        return (maxClose + 1) / 2;
+    }
+}
