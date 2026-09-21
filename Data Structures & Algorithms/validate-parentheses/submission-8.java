15631a3a94c7298fb1a34f0a2906be2d8982b914
Josh Kahlbaugh <joshk7683@gmail.com>
2026-09-20 18:59:22 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                char top = stack.peek();
+                if (c == '}' && top == '{') {
+                    stack.pop();
+                } else if (c == ']' && top == '[') {
+                    stack.pop();
+                } else if (c == ')' && top == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
