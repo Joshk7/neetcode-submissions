48ef306b9b24fcf70323b7e5b999e984b250da3c
Josh Kahlbaugh <joshk7683@gmail.com>
2026-09-22 21:14:52 -0700

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob1 = 0, rob2 = 0;
+        for (int num : nums) {
+            int tmp = Math.max(rob1, num + rob2);
+            rob2 = rob1;
+            rob1 = tmp;
+        }
+
+        return rob1;
+    }
+}
